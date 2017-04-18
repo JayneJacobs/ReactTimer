33b5ece5f2e609603005e6da2489f736c77e9313
jjacob004c <jayne_jacobs@cable.comcast.com>
2017-04-18 07:29:23 -0400

    Added controls

--- app/tests/components/Clock.test.jsx
+++ app/tests/components/Clock.test.jsx
@@ -1,5 +1,4 @@
 var React = require('react');
-
 var ReactDOM = require('react-dom');
 var expect = require('expect');
 var $ = require('jQuery');
@@ -15,8 +14,8 @@ describe('Clock', () => {
   describe('render', () => {
     it('should render clock to output', () => {
       var clock = TestUtils.renderIntoDocument(<Clock totalSeconds={62}/>);
-      var $el = $(ReactDOM.findDomNode(clock));
-      var actualText = $el.find('clock-text').text();
+      var $el = $(ReactDOM.findDOMNode(clock));
+      var actualText = $el.find('.clock-text').text();
 
       expect(actualText).toBe('01:02');
     });
@@ -27,19 +26,18 @@ describe('Clock', () => {
       var clock = TestUtils.renderIntoDocument(<Clock/>);
       var seconds = 615;
       var expected = '10:15';
-      var actual= clock.formatSeconds(seconds);
+      var actual = clock.formatSeconds(seconds);
 
       expect(actual).toBe(expected);
     });
 
-    it('should format seconds when min/sec less than 10', () => {
+    it('should format seconds when min/sec are less than 10', () => {
       var clock = TestUtils.renderIntoDocument(<Clock/>);
       var seconds = 61;
       var expected = '01:01';
-      var actual= clock.formatSeconds(seconds);
+      var actual = clock.formatSeconds(seconds);
 
       expect(actual).toBe(expected);
     });
-
   });
 });
--- app/tests/components/Countdown.test.jsx
+++ app/tests/components/Countdown.test.jsx
@@ -1,43 +1,64 @@
 var React = require('react');
+var Clock = require('Clock');
+var CountdownForm = require('CountdownForm');
+var Controls = require('Controls');
 
-var ReactDOM = require('react-dom');
-var expect = require('expect');
-var $ = require('jQuery');
-var TestUtils = require('react-addons-test-utils');
-
-var Countdown = require('Countdown');
-
-describe('Countdown', () => {
-  it('should exist', () => {
-    expect(Countdown).toExist();
-  });
-
-describe('handleSetCountdown', () => {
-  it('should set state to started and Countdown', (done) => {
-    var countdown = TestUtils.renderIntoDocument(<Countdown/>);
-    countdown.handleSetCountdown(10);
-
-    expect(countdown.state.count).toBe(10);
-    expect(countdown.state.countdownStatus).toBe('started');
-
-    setTimeout(() => {
-      expect(countdown.state.count).toBe(9);
-      done();
-    }, 1001)
-
-    });
-
-
-
-    it('should never set count to less than 0', (done) => {
-      var countdown = TestUtils.renderIntoDocument(<Countdown/>);
-      countdown.handleSetCountdown(1);
-
-    setTimeout( () => {
-          expect(countdown.state.count).toBe(0);
-          done();
-        }, 3001)
-    
+var Countdown = React.createClass({
+  getInitialState: function () {
+    return {
+      count: 0,
+      countdownStatus: 'stopped'
+    };
+  },
+  componentDidUpdate: function (prevProps, prevState) {
+    if (this.state.countdownStatus !== prevState.countdownStatus) {
+      switch (this.state.countdownStatus) {
+        case 'started':
+          this.startTimer();
+          break;
+        case 'stopped':
+          this.setState({count: 0});
+        case 'paused':
+          clearInterval(this.timer)
+          this.timer = undefined;
+          break;
+      }
+    }
+  },
+  startTimer: function () {
+    this.timer = setInterval(() => {
+      var newCount = this.state.count - 1;
+      this.setState({
+        count: newCount >= 0 ? newCount : 0
       });
+    }, 1000);
+  },
+  handleSetCountdown: function (seconds) {
+    this.setState({
+      count: seconds,
+      countdownStatus: 'started'
     });
+  },
+  handleStatusChange: function (newStatus) {
+    this.setState({countdownStatus: newStatus});
+  },
+  render: function () {
+    var {count, countdownStatus} = this.state;
+    var renderControlArea = () => {
+      if (countdownStatus !== 'stopped') {
+        return <Controls countdownStatus={countdownStatus} onStatusChange={this.handleStatusChange}/>;
+      } else {
+        return <CountdownForm onSetCountdown={this.handleSetCountdown}/>;
+      }
+    };
+
+    return (
+      <div>
+        <Clock totalSeconds={count}/>
+        {renderControlArea()}
+      </div>
+    );
+  }
 });
+
+module.exports = Countdown;
