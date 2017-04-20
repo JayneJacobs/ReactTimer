9e46c9ef39f5cdbd9a38ae8a40bcb0774376b7f1
jjacob004c <jayne_jacobs@cable.comcast.com>
2017-04-20 09:17:58 -0400

    completed timer

--- app/components/Timer.jsx
+++ app/components/Timer.jsx
@@ -1,8 +1,68 @@
 var React = require('react');
+var Clock = require('Clock');
+var TimerForm = require('TimerForm');
+var Controls = require('Controls');
 
 var Timer = React.createClass({
+  getInitialState: function () {
+    return {
+      count: 0,
+      timerStatus: 'stopped'
+    };
+  },
+  componentDidUpdate: function (prevProps, prevState) {
+    if (this.state.timerStatus !== prevState.timerStatus) {
+      switch (this.state.timerStatus) {
+        case 'started':
+          this.handleStart();
+          break;
+        case 'stopped':
+          this.setState({count: 0});
+        case 'paused':
+          clearInterval(this.timer)
+          this.timer = undefined;
+          break;
+      }
+    }
+  },
+  componentWillUnmount: function () {
+    clearInterval(this.timer);
+    this.timer = undefined;
+  },
+    handleStart: function () {
+    this.timer = setInterval(() => {
+      this.setState({
+        count: this.state.count + 1
+      });
+    }, 1000);
+  },
+  handleSetTimer: function (seconds) {
+    this.setState({
+      count: seconds,
+      timerStatus: 'started'
+    });
+  },
+  handleStatusChange: function (newTimerStatus) {
+    console.log(newTimerStatus);
+    this.setState({timerStatus: newTimerStatus});
+  },
   render: function () {
-    return <p>Timer.jsx</p>;
+    var {count, timerStatus} = this.state;
+    var renderControlArea = () => {
+      if (timerStatus !== 'stopped') {
+        return <Controls timerStatus={timerStatus} onStatusChange={this.handleStatusChange}/>;
+      } else {
+        return <TimerForm onSetTimer={this.handleSetTimer}/>;
+      }
+    };
+
+    return (
+      <div>
+        <h1 className="page-title">Timer App</h1>
+        <Clock totalSeconds={count}/>
+      <Controls countdownStatus={timerStatus}onStatusChange={this.handleStatusChange}/>
+      </div>
+    );
   }
 });
 
